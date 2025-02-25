a3928f17b57e613864dc9a0f1b45a3dda78b528d
Matheus Gouveia <maatheusgouveia@gmail.com>
2025-02-24 23:11:42 -0300

    implement authentication module with JWT support and user service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { APP_GUARD } from '@nestjs/core';
+
 import { AppService } from './app.service';
-import { ArticlesModule } from './modules/articles/articles.module';
+import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { ArticlesModule } from './modules/articles/articles.module';
 
 @Module({
   imports: [ArticlesModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.enableCors();
 
   await app.listen(3333);
