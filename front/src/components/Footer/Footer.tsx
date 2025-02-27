a89a6338d8d44adecd2d7122efc8c8bb18d931be
Matheus Gouveia <maatheusgouveia@gmail.com>
2025-02-26 23:34:54 -0300

    add internationalization support to Header and Footer components; update translation files for English and Portuguese

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,9 +1,13 @@
+import { useTranslations } from "next-intl";
+
 export function Footer() {
+	const t = useTranslations("footer");
+
 	return (
 		<footer className="flex justify-between items-center p-4 bg-primary h-32 px-32">
 			<small>
-				Feito com <span className="text-red-700 mr-1">❤</span>
-				por@maatheusgouveia
+				{t("made with")} <span className="text-red-700 mr-1">❤</span>
+				{t("by")} @maatheusgouveia
 			</small>
 
 			<small>Powered by Warlocks Tecnologia</small>
--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useLocale, useTranslations } from "next-intl";
+import { Locale } from "@/i18n/config";
+import { setUserLocale } from "@/services/locale";
+import { Select, SelectItem } from "@heroui/react";
 import { useAuthentication } from "@/contexts/AuthContext";
 
 export function Header() {
+	const locale = useLocale();
 	const { logout, auth } = useAuthentication();
-
 	const { isAuthenticated } = auth;
+	const t = useTranslations("header");
 
 	function handleLogout() {
 		logout();
@@ -14,13 +19,32 @@ export function Header() {
 	return (
 		<header className="flex justify-between items-center p-4 h-16 px-32 bg-primary font-bold">
 			<h1 className="font-bold">Warlocks</h1>
-			<ul className="flex gap-4">
-				<li>Inscreva-se</li>
+			<ul className="flex gap-4 justify-center items-center">
+				<li>{t("subscribe")}</li>
 				{isAuthenticated && (
 					<li className="cursor-pointer" onClick={handleLogout}>
-						Sair
+						{t("logout")}
 					</li>
 				)}
+				<li>
+					<Select
+						defaultSelectedKeys={[locale]}
+						value={locale}
+						onChange={(e) =>
+							setUserLocale(e.target.value as Locale)
+						}
+						aria-label="Select locale"
+						className="min-w-32 text-white"
+						size="lg"
+					>
+						<SelectItem className="text-white" key="en">
+							English
+						</SelectItem>
+						<SelectItem className="text-white" key="pt-BR">
+							Português
+						</SelectItem>
+					</Select>
+				</li>
 			</ul>
 		</header>
 	);
